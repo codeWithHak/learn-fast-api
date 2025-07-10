317d13536339dc755a7998090a5ccf5708878d58
codeWithHak <huzairahmedkhan@gmail.com>
2025-07-10 20:31:36 +0500

    complete file validator

---
+++ 01-fastapi-awfera/projects/02-image-size-validator/main.py
@@ -0,0 +1,13 @@
+# from fastapi import FastAPI, File, UploadFile
+# import uvicorn
+
+
+# app = FastAPI(
+#     title="Basic Image Validation",
+#     description="Verifies the image and tell us if size is under 200mb",
+#     version="0.116.0"
+# )
+
+# @app.post("validate-image")
+# def validate_image(image:)
+
