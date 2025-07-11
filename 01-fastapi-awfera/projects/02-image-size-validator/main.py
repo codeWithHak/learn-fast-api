7c3c574c1549ffd221ec0e5897aa21dddf50cc41
codeWithHak <huzairahmedkhan@gmail.com>
2025-07-11 13:41:51 +0500

    complete file validator

--- 01-fastapi-awfera/projects/02-image-size-validator/main.py
+++ 01-fastapi-awfera/projects/02-image-size-validator/main.py
@@ -1,13 +1,29 @@
-# from fastapi import FastAPI, File, UploadFile
-# import uvicorn
+from typing import Annotated
+
+from fastapi import FastAPI, File
+
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def read_route():
+    return "Hello"
 
 
-# app = FastAPI(
-#     title="Basic Image Validation",
-#     description="Verifies the image and tell us if size is under 200mb",
-#     version="0.116.0"
-# )
+@app.post("/files/")
 
-# @app.post("validate-image")
-# def validate_image(image:)
+# we set the type of data to File(...) which means now fatapi will expect a file (multipart/formdata), by default it expects a string.
 
+async def create_file(file:bytes = File(...)):
+    length = len(file) / (1024 * 1024)
+    
+    message = "Valid file" if length <= 2  else "File is too large"
+    response = {
+        "message":message,
+        "length": f"{length:.2f} mb"
+    }
+    return response
+
+if __name__ == "__main__":
+    uvicorn.run(app="test:app", host="127.0.0.1" ,port=800, reload=True)
