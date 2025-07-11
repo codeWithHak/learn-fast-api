5b672efa849c48411b80d63c6e5c3d73f644309c
codeWithHak <huzairahmedkhan@gmail.com>
2025-07-11 20:16:34 +0500

    learn basic routes

--- 01-fastapi-awfera/04-routers/main.py
+++ 01-fastapi-awfera/04-routers/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from 04-routers!")
+from fastapi import FastAPI
+import uvicorn
+from app_router import page_router  
+app = FastAPI(title="Basic Router")
+
+
+app.include_router(page_router)
+
+@app.get("/")
+def read_route():
+    return {"message":"Welcome To Root"}
+
+
+@app.get("/shop")
+def shop():
+    return {"message":"Welcome To Shop Page"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", port=600, reload=True)
