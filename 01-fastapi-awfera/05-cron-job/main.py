02d5e8576579a2ce7397207b38d2c9eb64493bd4
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-07-17 15:11:54 +0500

    complete advance cronjob

--- 01-fastapi-awfera/05-cron-job/main.py
+++ 01-fastapi-awfera/05-cron-job/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from datetime import datetime
 import uvicorn
 from enum import Enum
-
+from apscheduler.triggers.cron import CronTrigger
 
 app = FastAPI(title="Basic Cronjob", description="Apply basic cron job functionality using fastapi")
 
@@ -14,26 +14,38 @@ scheduler.start()
 def send_reminder():
     print(f"Reminder is sent at: {datetime.now()}")
 
-class Weekdays (str,Enum):
-    mon = "mon"
-    tue = "tue"
-    wed = "wed"
-    thu = "thu"
-    fri = "fri"
-    sat = "sat"
-    sun = "sun"
+# class Weekdays (str,Enum):
+#     mon = "mon"
+#     tue = "tue"
+#     wed = "wed"
+#     thu = "thu"
+#     fri = "fri"
+#     sat = "sat"
+#     sun = "sun"
 
 
 class TimeInput(BaseModel):
-    day:Weekdays = Field(...)
+
+    month:str = Field(default="1,2,3,4,5,6,7,8,9,10,11,12")
+    day:str = Field(default="mon,tue,wed,thu,fri,sat,sun")
     hour:int = Field(..., ge=0, le=23)
     minute:int = Field(..., ge=0, le=59)
 
 
 @app.post("/send-reminder-route")
 def send_reminder_route(time:TimeInput):
-    scheduler.add_job(send_reminder, trigger="cron", day_of_week=time.day.value ,hour=time.hour, minute=time.minute)
-    return {"message":f"Request will be sent at {time.hour}:{time.minute} on {time.day}"}
+
+    cron_args = {
+    "month":time.month,
+    "day_of_week":time.day,
+    "hour":time.hour,
+    "minute":time.minute
+}
+
+    cron_args = {k:v for k,v in cron_args.items() if v is not None}
+    print(cron_args)
+    scheduler.add_job(send_reminder, CronTrigger(**cron_args))
+    return {"message":f"Request will be sent at {time.hour}:{time.minute}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=800, reload=True)
